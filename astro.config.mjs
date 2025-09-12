ea10726968780010836a4d3b18abbbf660ddc241
Ziel TUF <zielalfinoakbar@gmail.com>
2025-09-12 16:50:26 +0700

    i ges done here

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon()]
 });
